e3094c94df34c3bd1ac524a2e2922b3d287f25b8
YMV <RealYMV@users.noreply.github.com>
2018-12-02 09:54:47 +0200

    яс

---
+++ index.php
@@ -0,0 +1,236 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content=" .">
+  <meta name="author" content="">
+  <link rel="icon" href="../../../../favicon.ico">
+
+  <title>Натяжные потолки в Германии</title>
+
+  <!-- Bootstrap core CSS-->
+  <!--link href="/css/bootstrap.css" rel="stylesheet"-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  <!-- Custom styles for this template -->
+  <link href="css/carousel.css" rel="stylesheet">
+</head>
+<body>
+
+
+
+  <header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+
+    <a class="navbar-brand" href="#">
+      <img src="logo.png" width="90" height="50" alt="">
+    </a>
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">      <img src="home.png" > <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">О нас</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Галерея</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Натяжной потолок в ванную комнату</a>
+                <a class="dropdown-item" href="#">Натяжной потолок в обеденную комнату</a>
+                <a class="dropdown-item" href="#">Натяжной потолок в коридор</a>
+                <a class="dropdown-item" href="#">Натяжной потолок в детскую комнату</a>
+                <a class="dropdown-item" href="#">Натяжной потолок на кухню</a>
+                <a class="dropdown-item" href="#">Натяжной потолок в спальню</a>
+                <a class="dropdown-item" href="#">Натяжной потолок в зал</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">    </a>
+            </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Цены</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Рассчитать</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Блог</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Impressum</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Контакты</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="tel:0152-56659112">0152-56659112</a>
+                <a class="dropdown-item" href="tel:06359-923984">06359-923984</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="mailto:info@luxus-spanndecken.de"> info@luxus-spanndecken.de</a>
+              </div>
+            </li>
+
+        </ul>
+        <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class=" " href="#">      <img src="ru_ru.gif"  alt=""> </a>
+        <a class=" " href="#">      <img src="de_de.gif"  alt=""> </a>
+  </li>
+
+      </div>
+    </nav>
+  </header>
+
+  <main role="main">
+
+    <div id="myCarousel" class="carousel slide  carousel-fade" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="first-slide img-fluid" src="/slaider/77.jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption text-left">
+              <p class="h2 text-center font-weight-bold">Шикарные потолки - для вас!</p>
+              <p class="text-center font-weight-bold">Нет такого помещения, которое мы не смогли бы сделать более красивым!</p>
+
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="second-slide img-fluid" src="/slaider/3.jpg" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <p class="h2 text-light font-weight-bold">У нас много креативных идей!</p>
+ </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="third-slide image-fluid" src="/slaider/4_cr.jpg" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption text-right">
+              <p class="h2 text-center">Гарантия высокого качества!</p>
+              <p class="text-center font-weight-bold">Вы будете довольны своим выбором!</p>
+              <p><a class="btn btn-lg btn-outline-success" href="#" role="button">Посмотреть</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+  </main>
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+<div class="container">
+<h1 class="text-center">Натяжные потолки в Германии</h1>
+<p class="h3 text-center" > Здравствуйте, уважаемые посетители!</p>
+<p>Вы только зашли на наш сайт, а мы уже знаем, что вам нужно! Очевидно: натяжные потолки в Германии! Но к ним вы получаете:
+<ol>
+  <li> Обслуживание высокого класса: вам доступен бесплатный расчет цен на натяжные потолки с установкой. Кроме этого, мы располагаем большим количеством дизайнерских решений, которые подходят для всех помещений. В нашей команде для вас работают специалисты высокого класса с большим опытом работы. Мы гарантируем, что результат работы будет впечатляющим, а эффект - постоянным.</li>
+  <li> Экологичность, надежность! Используем только качественные, экологически чистые материалы, поэтому натяжные потолки не только безвредны, но и способствуют здоровому микроклимату!  Оперативность и пунктуальность. Этим все сказано.</li>
+  <li> Эффективная коммуникация. От вас не требуется хорошо понимать нюансы проектирования и как осуществляется установка, монтаж натяжных потолков - мы предоставляем всю информацию понятно и отвечаем на все вопросы клиентов.</li>
+</ol>  </p>
+<p> Еще больше преимуществ вы узнаете, сделав бесплатный расчет цен на натяжные потолки и заказ на установку натяжных потолков.
+  Можно уже сейчас проконсультироваться с нашим специалистом, он сам сделает нужные расчеты - так вы сэкономите не только деньги, но и время! Мы также можем обговорить проект при личной встрече! У нас много клиентов, но каждый клиент для нас - особенный! Натяжные потолки в Германии с установкой от нашей компании - отличное решение для частных квартир, офисов, кабинетов руководителя, приемных - словом, для любого типа помещений! А теперь предлагаем вам просмотреть наш каталог или сразу сделать заявку на бесплатный расчет натяжного потолка.</p>
+</div>
+
+    <div class="container marketing">
+
+
+      <!-- Three columns of text below the carousel -->
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Еще пару слов... <span class="text-muted">если вы сомневаетесь:</span></h2>
+          <p class="lead">С самого начала нашего контакта вы будете довольны нами, нашим профессионализмом и творческим подходом! Если желаете, мы предоставим вам возможность оценить наши работы на объектах. </p>
+        </div>
+        <div class="col-md-5">
+<p>          <img class="featurette-image img-fluid mx-auto" src="slaider/4-ff7131cb1d.JPG" alt="Generic placeholder image"></p>
+  <p>                  <img class="featurette-image img-fluid mx-auto" src="slaider/33.jpg" alt="Generic placeholder image"><br/>
+    <p>                          <img class="featurette-image img-fluid mx-auto" src="slaider/8.jpg" alt="Generic placeholder image"><br/>
+         </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 order-md-2">
+          <h2 class="featurette-heading">Просим обратить внимание <span class="text-muted">на то, что:</span></h2>
+          <p class="lead">Просмотр всех наших работ заняло бы очень много времени, поэтому мы предлагаем пообщаться с нашим представителем, чтобы мы подобрали дизайн натяжного потолка индивидуально.</p>
+        </div>
+        <div class="col-md-5 order-md-1">
+      <p>    <img class="featurette-image img-fluid mx-auto" src="slaider/4_cr.jpg" alt="Generic placeholder image"><br/>
+        <p>  <img class="featurette-image img-fluid mx-auto" src="slaider/33.jpg" alt="Generic placeholder image"><br/>
+          <p><img class="featurette-image img-fluid mx-auto" src="slaider/8.jpg" alt="Generic placeholder image"><br/>
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+          <img class="featurette-image img-fluid mx-auto" src="master1-ru.jpg" alt="Generic placeholder image">
+        <div class="col-md-7">
+
+      </div>
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
+
+    </div><!-- /.container -->
+
+
+    <!-- FOOTER -->
+    <footer class="container">
+      <p class="float-right"><a href="#">Вверх</a></p>
+      <p>&copy; 2018 Luxus Spanndecken&middot; <a href="#">Политика конфиденциальности</a> &middot; <a href="#">О "cookies"</a></p>
+    </footer>
+   <script src="/js/carousel.js"></script>
+    <script src="jquery-3.3.1.slim.min.js" ></script>
+    <script src="popper.min.js"></script>
+  <script src="/js/bootstrap.js"></script>
+
+
+
+</body>
+</html>
---
+++ natjazhnoy-potolok-v-vannuju.php
@@ -0,0 +1,197 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="elsl-css/elastislide.css" />
+  <link rel="stylesheet" type="text/css" href="elsl-css/custom.css" />
+	<script src="elsl-js/modernizr.custom.17475.js"></script>
+
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content=" .">
+  <meta name="author" content="">
+  <link rel="icon" href="../../../../favicon.ico">
+
+  <title>Натяжные потолки в Германии</title>
+
+  <!-- Bootstrap core CSS-->
+  <link href="/css/bootstrap.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/carousel.css" rel="stylesheet">
+</head>
+<body>
+
+
+
+  <header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+
+    <a class="navbar-brand" href="#">
+      <img src="logo.png" width="90" height="50" alt="">
+    </a>
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">      <img src="home.png" > <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">О нас</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Галерея</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Натяжной потолок в ванную комнату</a>
+                <a class="dropdown-item" href="#">Натяжной потолок в обеденную комнату</a>
+                <a class="dropdown-item" href="#">Натяжной потолок в коридор</a>
+                <a class="dropdown-item" href="#">Натяжной потолок в детскую комнату</a>
+                <a class="dropdown-item" href="#">Натяжной потолок на кухню</a>
+                <a class="dropdown-item" href="#">Натяжной потолок в спальню</a>
+                <a class="dropdown-item" href="#">Натяжной потолок в зал</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">    </a>
+            </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Цены</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Рассчитать</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Блог</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Impressum</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">    </a>
+          </li>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Контакты</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="tel:0152-56659112">0152-56659112</a>
+                <a class="dropdown-item" href="tel:06359-923984">06359-923984</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="mailto:info@luxus-spanndecken.de"> info@luxus-spanndecken.de</a>
+              </div>
+            </li>
+
+        </ul>
+        <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class=" " href="#">      <img src="ru_ru.gif"  alt=""> </a>
+        <a class=" " href="#">      <img src="de_de.gif"  alt=""> </a>
+  </li>
+
+      </div>
+    </nav>
+  </header>
+     <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+<div class="container">
+<h1 class="text-center">Натяжной потолок в ванную - хорошее решение?</h1>
+<p>Ванная комната относится к помещению, где уровень влажности нестабильный и часто очень высокий. Это отражается на выборе тех материалов, которые используются для ремонта и строительства. Учитывая это, многие считают, что нужно выбирать только практичные решения, которые не имеют привлекательного дизайна, которые не являются декоративными. Главное - влагоустойчивость. Но мы предлагаем вам не искать компромисс, а заказать у нас натяжной потолок в ванную, здесь есть и практичность, и красота! На данный момент мы располагаем всем необходимым: материалами, имеющими ряд преимуществ, как в визуальных, так и в технических характеристиках, высококлассными специалистами, технической базой для наиболее качественного предоставления услуг по дизайну, установке и обслуживанию натяжных потолков в ванную. </p>
+<p>Кстати, а каким должен быть натяжной потолок в ванную?</p>
+<p>Он в целом должен иметь высокую <mark>влагоустойчивость</mark> - факт <span class="text-success">первый</span>.</p>
+<p>Он должен <mark>обеспечивать защиту</mark> осветительных приборов, расположенных непосредственно в конструкции - факт <span class="text-warning">второй</span>. </p>
+<p>Он должен обладать <mark>отличными визуальными характеристиками</mark>, приносить эстетическое удовольствие даже тогда, когда вы не смотрите непосредственно на него - факт <span class="text-primary">третий</span>.</p>
+<p>Он должен составлять с другими элементами дизайна интерьера целый ансамбль, быть <mark>неотъемлемой его частью</mark> - факт <span class="text-danger">четвертый</span>. </p>
+<p>Он <mark>не должен</mark> требовать постоянного и тщательного ухода или специальных очищающих средств - факт <span class="text-dark">пятый</span>.</p>
+<p>Добавим, что натяжные потолки в ванную должны отвечать <mark>строгим экологическим и санитарно-гигиеническим нормам</mark>. Лучше, если компания по установке натяжных потолков в ванную вообще не использует сомнительного качества материалы. Как мы, например. Прибавьте к этому большой опыт, который имеют наши сотрудники, гарантию качества работы и решение заказать у нас натяжные потолки в ванную станет очевидным, наиболее рациональным решением!</p>
+</div>
+<div class="container fluid col-md-6">
+<!-- Elastislide Carousel -->
+<ul id="carousel" class="elastislide-list">
+ <li data-preview="vanna/1.jpg"><a href="#"><img src="elsl-images/small/1.jpg" alt="image04" /></a></li>
+ <li data-preview="vanna/2.jpg"><a href="#"><img src="elsl-images/small/2.jpg" alt="image05" /></a></li>
+ <li data-preview="vanna/3.jpg"><a href="#"><img src="elsl-images/small/3.jpg" alt="image06" /></a></li>
+ <li data-preview="vanna/4.jpg"><a href="#"><img src="elsl-images/small/4.jpg" alt="image07" /></a></li>
+ <li data-preview="vanna/5.jpg"><a href="#"><img src="elsl-images/small/5.jpg" alt="image11" /></a></li>
+ <li data-preview="vanna/6.jpg"><a href="#"><img src="elsl-images/small/6.jpg" alt="image12" /></a></li>
+ <li data-preview="vanna/7.jpg"><a href="#"><img src="elsl-images/small/7.jpg" alt="image13" /></a></li>
+ <li data-preview="vanna/8.jpg"><a href="#"><img src="elsl-images/small/8.jpg" alt="image14" /></a></li>
+</ul>
+<!-- End Elastislide Carousel -->
+
+<div class="image-preview">
+ <img id="preview" src="vanna/1.jpg" />
+</div>
+</div>
+
+</div>
+
+
+
+    </div><!-- /.container -->
+
+
+    <!-- FOOTER -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="/js/bootstrap.js"></script>
+  <script type="text/javascript" src="elsl-js/jquerypp.custom.js"></script>
+		<script type="text/javascript" src="elsl-js/jquery.elastislide.js"></script>
+		<script type="text/javascript">
+
+			// example how to integrate with a previewer
+
+			var current = 0,
+				$preview = $( '#preview' ),
+				$carouselEl = $( '#carousel' ),
+				$carouselItems = $carouselEl.children(),
+				carousel = $carouselEl.elastislide( {
+					current : current,
+					minItems : 4,
+					onClick : function( el, pos, evt ) {
+
+						changeImage( el, pos );
+						evt.preventDefault();
+
+					},
+					onReady : function() {
+
+						changeImage( $carouselItems.eq( current ), current );
+
+					}
+				} );
+
+			function changeImage( el, pos ) {
+
+				$preview.attr( 'src', el.data( 'preview' ) );
+				$carouselItems.removeClass( 'current-img' );
+				el.addClass( 'current-img' );
+				carousel.setCurrent( pos );
+
+			}
+
+		</script>
+
+
+    <footer class="container">
+      <p class="float-right"><a href="#">Вверх</a></p>
+      <p>&copy; 2018 Luxus Spanndecken&middot; <a href="#">Политика конфиденциальности</a> &middot; <a href="#">О "cookies"</a></p>
+    </footer>
+
+</body>
+</html>
